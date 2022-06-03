8896f2e203825f6c8a868f74e257ed5f7f0c4497
Giovanni Claro <gio.claro316@gmail.com>
2022-06-03 00:09:28 -0400

    added more tests

--- _tests_/Player.test.js
+++ _tests_/Player.test.js
@@ -13,3 +13,25 @@ test('creates a player object', () => {
     expect(player.agility).toEqual(expect.any(Number));
     expect(player.inventory).toEqual(expect.arrayContaining([expect.any(Object)]));
   });
+
+  test("gets player's stats as an object", () => {
+    const player = new Player('Dave');
+  
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+    //Here, we're checking that player.getStats() returns an object with four specific properties.
+  });
+
+  test('gets inventory from player or returns false', () => {
+    const player = new Player('Dave');
+  
+    expect(player.getInventory()).toEqual(expect.any(Array));
+  
+    player.inventory = [];
+  
+    expect(player.getInventory()).toEqual(false);
+    //On player creation, the inventory should already have something in it, so a call to player.getInventory() should return an array. 
+    //There's also the case of an empty inventory needing to return false. 
+  });
